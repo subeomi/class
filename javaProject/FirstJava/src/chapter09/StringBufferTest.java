3bae6e798589be07c950065160f67be60f5f2bf4
subeomi <tnqjarnqja@gmail.com>
2023-04-03 18:20:02 +0900

    230403

---
+++ javaProject/FirstJava/src/chapter09/StringBufferTest.java
@@ -0,0 +1,40 @@
+package chapter09;
+
+public class StringBufferTest {
+
+	public static void main(String[] args) {
+		
+		StringBuffer sb1 = new StringBuffer();
+		System.out.println(sb1.capacity());
+		
+		StringBuffer sb = new StringBuffer("Hello");
+		System.out.println(sb);
+		System.out.println(sb.capacity());
+		
+		// append
+//		sb.append('~');     // "Hello~"
+//		sb.append(" JAVA"); // "Hello~ JAVA"
+		sb.append('~').append(" JAVA"); // 체이닝. 연결해서 처리
+		System.out.println(sb);
+		
+		// delete
+		sb.delete(5, 7); // 5~(7-1)
+		System.out.println(sb);
+		
+		// insert
+		sb.insert(5, '~');
+		System.out.println(sb);
+		sb.insert(6, "~! ");
+		System.out.println(sb);
+		sb.insert(sb.length(), false);
+		System.out.println(sb); // Hello~~! JAVAfalse
+		
+		// reverse
+		sb.reverse();
+		System.out.println(sb);
+		
+		String result = new String(sb);
+		
+	}
+	
+}

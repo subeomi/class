1f43e032d2191e687c677c0e90134112b333eaf5
subeomi <tnqjarnqja@gmail.com>
2023-04-07 18:17:30 +0900

    230407

---
+++ javaProject/FirstJava/src/chapter12/SerializableTest.java
@@ -0,0 +1,34 @@
+package chapter12;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+
+public class SerializableTest {
+
+	public static void main(String[] args) {
+		Person person = new Person("KING", 20);
+		String msg = "안녕하세요";
+		
+		ObjectInputStream inputStream = null;
+		
+		try {
+			FileInputStream in = new FileInputStream("instanceData.ser");
+			
+			inputStream = new ObjectInputStream(in);
+			
+//			Person p = (Person) inputStream.readObject();
+			Person p = (Person) inputStream.readObject();
+			String newStr = (String) inputStream.readObject();
+			
+			p.tell();
+			System.out.println(newStr);
+			
+		} catch (IOException | ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+
+}

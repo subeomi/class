fa43222d5ddf6af6321f092bcee3c3ccccee0dd5
subeomi <tnqjarnqja@gmail.com>
2023-05-01 18:28:45 +0900

    todo

--- jspProject/Todo/src/main/java/todo/controller/TodoListController.java
+++ jspProject/Todo/src/main/java/todo/controller/TodoListController.java
@@ -1,6 +1,7 @@
 package todo.controller;
 
 import java.io.IOException;
+import java.util.List;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -9,10 +10,18 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import todo.domain.TodoDTO;
+import todo.service.TodoListService;
+
 @WebServlet(name = "todoListController", urlPatterns = "/todo/list")
 public class TodoListController extends HttpServlet{
 
-
+	TodoListService listService;
+	
+	public TodoListController() {
+		this.listService = new TodoListService();
+	}
+	
 	// 화면에 리스트 출력 : get 방식의 요청
 	// 브라우저의 url창에 입력해서 요청 -> get
 	
@@ -28,7 +37,11 @@ public class TodoListController extends HttpServlet{
 		
 		// 1. 사용자 요청의 분석
 		// 2. Service에 요청 -> 응답 데이터 반환
+		List<TodoDTO> list = listService.getList();
+		
 		// 3. 응답 데이터 request의 속성에 저장
+		request.setAttribute("todoList", list);
+		
 		// 4. view 지정 -> forward 
 		
 		String viewPath = "/WEB-INF/views/todo/list.jsp";
